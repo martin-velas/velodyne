d93914faf289016a1b347927503f3f0194adb4c2
Martin Velas <mato.velas@gmail.com>
2018-01-31 12:51:51 +0100

    PointXYRIR preserved

--- velodyne_pointcloud/include/velodyne_pointcloud/point_types.h
+++ velodyne_pointcloud/include/velodyne_pointcloud/point_types.h
@@ -34,6 +34,15 @@ struct PointXYZIRP
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW     // ensure proper alignment
 } EIGEN_ALIGN16;
 
+/** Euclidean Velodyne coordinate, including intensity and ring number. */
+struct PointXYZIR
+{
+  PCL_ADD_POINT4D;                    // quad-word XYZ
+  float    intensity;                 ///< laser intensity reading
+  uint16_t ring;                      ///< laser ring number
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW     // ensure proper alignment
+} EIGEN_ALIGN16;
+
 }; // namespace velodyne_pointcloud
 
 POINT_CLOUD_REGISTER_POINT_STRUCT(velodyne_pointcloud::PointXYZIRP,
@@ -43,6 +52,13 @@ POINT_CLOUD_REGISTER_POINT_STRUCT(velodyne_pointcloud::PointXYZIRP,
                                   (float, intensity, intensity)
                                   (uint16_t, ring, ring)
                                   (float, phase, phase))
+
+POINT_CLOUD_REGISTER_POINT_STRUCT(velodyne_pointcloud::PointXYZIR,
+                                  (float, x, x)
+                                  (float, y, y)
+                                  (float, z, z)
+                                  (float, intensity, intensity)
+                                  (uint16_t, ring, ring))
 
 typedef velodyne_pointcloud::PointXYZIRP VPoint;
 typedef pcl::PointCloud<VPoint> VPointCloud;
